25f483caebd7c4416c591e59284470dfc505fefb
Luiz Eduardo <Luiz Eduardo@192.168.0.14>
2014-11-05 15:23:45 -0200

    Removendo Ctrl-c, Ctrl-v

--- src/module1/pechincha/view/Question.java
+++ src/module1/pechincha/view/Question.java
@@ -13,13 +13,15 @@ public class Question extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+/*		Bora reduzir linhas né
 		ServletController sc = new ServletController(getServletContext().getRealPath("/"));
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
 		out.print(sc.process(request)); 
 		out.close();
-	}
+*/	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ServletController sc = new ServletController(getServletContext().getRealPath("/"));
