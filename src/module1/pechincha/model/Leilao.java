9a6e09401cd8d664129a29b29fc7486025fe13dd
erickstryck <erickstryck@hotmail.com>
2014-11-06 15:21:03 -0200

    Alterações no Modelo de leilão e criação das classe base do gerenciar leilão, Allysom caso alterações no modelo leilão impactar no funcionamento dos seus componente favor falar comigo.

--- src/module1/pechincha/model/Leilao.java
+++ src/module1/pechincha/model/Leilao.java
@@ -1,32 +1,81 @@
 package module1.pechincha.model;
 
+import java.util.ArrayList;
+
 import module1.pechincha.util.ReflectiveModel;
 
 public class Leilao extends ReflectiveModel {
-	private int id;
+	private int idLeilao;
 	private String etiqueta; 
 	private int tempoLimite;
 	private int idLeiloeiro; 
 	private float lanceInicial;
+	private String descricao;
+	private int comprador;
+	private ArrayList<LoteProduto> produto;
+	private String nickname;
+	private boolean ativo;
 	
-	public Leilao(int id, String etiqueta, int tempoLimite, int idLeiloeiro, float lanceInicial){
-		this.id = id; 
+	public Leilao(int idLeilao, String etiqueta, int tempoLimite, int idLeiloeiro, float lanceInicial,String descricao,int comprador, ArrayList<LoteProduto> produto,String nickname,boolean ativo){
+		this.idLeilao = idLeilao; 
 		this.etiqueta = etiqueta;
 		this.tempoLimite = tempoLimite; 
 		this.setLanceInicial(lanceInicial);
-		this.idLeiloeiro = idLeiloeiro; 
+		this.idLeiloeiro = idLeiloeiro;
+		this.descricao=descricao;
+		this.produto=produto;
+		this.nickname=nickname;
+		this.ativo=ativo;
 	};
 	
-	public Leilao(int id){
-		this.id = id;
+	public Leilao(int idLeilao){
+		this.idLeilao = idLeilao;
 	}
-	public int getId() {
-		return id;
+	
+	
+	public ArrayList<LoteProduto> getProduto() {
+		return produto;
+	}
+	public void setProduto(ArrayList<LoteProduto> produto) {
+		this.produto = produto;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	public int getIdLeilao() {
+		return idLeilao;
+	}
+	
+	public void setIdLeilao(int idLeilao) {
+		this.idLeilao = idLeilao;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public String getDescricao() {
+		return descricao;
 	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getComprador() {
+		return comprador;
+	}
+
+	public void setComprador(int comprador) {
+		this.comprador = comprador;
+	}
+	
 	public int getIdLeiloeiro() {
 		return idLeiloeiro;
 	}
---
+++ src/module1/pechincha/viewUseCases/GerenciarLeilao.java
@@ -0,0 +1,19 @@
+package module1.pechincha.viewUseCases;
+
+import module1.pechincha.util.ActionDone;
+import module1.pechincha.view.ViewController;
+
+public class GerenciarLeilao extends ViewController{
+
+	public GerenciarLeilao(String sevletContext, String useCase) {
+		super(sevletContext, useCase);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String choose(ActionDone ad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
