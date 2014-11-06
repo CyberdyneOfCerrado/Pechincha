49eadc06b4d77f64d65d6479925b9731ff30146f
Luiz Eduardo <Luiz Eduardo@192.168.0.14>
2014-11-06 10:12:12 -0200

    Escrever alguma coisa aqui pq precisa

--- src/module1/pechincha/useCases/ManterProdutos.java
+++ src/module1/pechincha/useCases/ManterProdutos.java
@@ -1,16 +1,67 @@
+// Espero que isto aqui me ajude a upar arquivos http://www.tutorialspoint.com/servlets/servlets-file-uploading.htm
+
 package module1.pechincha.useCases;
 
 import module1.pechincha.controllers.ModelController;
+import module1.pechincha.util.ActionDone;
+import module1.pechincha.util.DoAction;
 
 public class ManterProdutos  extends ModelController{
-
+	public ActionDone novo ( DoAction da ){
+		ActionDone ad = new ActionDone();
+		
+		//Identificando o pacote
+		ad.setAction(da.getAction());
+		ad.setUseCase(da.getUseCase());
+		ad.setMessage("O Lula só tem quatro dedos em uma das mãos.");
+		ad.setStatus(true);
+		ad.setProcessed(true);
+		return ad;
+	};
+	
+	public ActionDone editar ( DoAction da ){
+		ActionDone ad = new ActionDone();
+		
+		//Identificando o pacote
+		ad.setAction(da.getAction());
+		ad.setUseCase(da.getUseCase());
+		ad.setMessage("O Lula só tem quatro dedos em uma das mãos.");
+		ad.setStatus(true);
+		ad.setProcessed(true);
+		return ad;
+	};
+	
+	public ActionDone remover ( DoAction da ){
+		ActionDone ad = new ActionDone();
+		
+		//Identificando o pacote
+		ad.setAction(da.getAction());
+		ad.setUseCase(da.getUseCase());
+		ad.setMessage("O Lula só tem quatro dedos em uma das mãos.");
+		ad.setStatus(true);
+		ad.setProcessed(true);
+		return ad;
+	};
+	
+	public ActionDone buscar ( DoAction da ){
+		ActionDone ad = new ActionDone();
+		
+		//Identificando o pacote
+		ad.setAction(da.getAction());
+		ad.setUseCase(da.getUseCase());
+		ad.setMessage("O Lula só tem quatro dedos em uma das mãos.");
+		ad.setStatus(true);
+		ad.setProcessed(true);
+		return ad;
+	};
+	
 	@Override
 	public String[] getActions() {
 		String[] actions = {
-			"",
-			"",
-			"",
-			""
+			"novo",
+			"editar",
+			"remover",
+			"buscar"
 		}; 
 		return actions;
 	}
