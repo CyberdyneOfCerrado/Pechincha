77e1ee6aa7ad8a4bb799112294077ffc1680a92f
erickstryck <erickstryck@hotmail.com>
2014-11-11 10:53:11 -0200

    Metodo de envio de email finalizado, JDBC usuario criado, modelo de usuario criado

--- src/module1/pechincha/cruds/JDBCLeilaoDAO.java
+++ src/module1/pechincha/cruds/JDBCLeilaoDAO.java
@@ -1,12 +1,18 @@
 package module1.pechincha.cruds;
 
+import java.sql.Connection;
 import java.util.List;
 
 import module1.pechincha.interf.DAOBehavior;
 import module1.pechincha.model.Leilao;
+import module1.pechincha.util.ConnectionFactory;
 
 public class JDBCLeilaoDAO extends DAOBehavior<Leilao>{
-
+	private Connection c; 
+	
+	public JDBCLeilaoDAO(){
+		c = ConnectionFactory.getConnection();
+	};
 	@Override
 	public void insert(Leilao arg) {
 		// TODO Auto-generated method stub
@@ -27,7 +33,7 @@ public class JDBCLeilaoDAO extends DAOBehavior<Leilao>{
 
 	@Override
 	public Leilao search(int pk) {
-		// TODO Auto-generated method stub
+		String sql="";
 		return null;
 	}
 
