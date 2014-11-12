e4160fe53f7b71c421f217470f504a46a934aaf0
erickstryck <erickstryck@hotmail.com>
2014-11-11 23:17:05 -0200

    Todos os produtos de um leilao implementado

--- src/module1/pechincha/cruds/JDBCLoteProdutoDAO.java
+++ src/module1/pechincha/cruds/JDBCLoteProdutoDAO.java
@@ -1,11 +1,16 @@
 package module1.pechincha.cruds;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import module1.pechincha.interf.DAOBehavior;
 import module1.pechincha.model.LoteProduto;
 import module1.pechincha.model.Produto;
 public class JDBCLoteProdutoDAO extends DAOBehavior<LoteProduto> {
-
+	private Connection c; 
 	@Override
 	public void insert(LoteProduto arg) {
 		// TODO Auto-generated method stub
@@ -20,7 +25,6 @@ public class JDBCLoteProdutoDAO extends DAOBehavior<LoteProduto> {
 
 	@Override
 	public List<LoteProduto> list() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -36,8 +40,30 @@ public class JDBCLoteProdutoDAO extends DAOBehavior<LoteProduto> {
 		
 	}
 	
-	public void produtosLeilao(int idleilao) {
-		Produto tempProd;
+	public List<Produto> produtosLeilao(int idleilao) {
+		List<Produto> list = new ArrayList<Produto>();
+		
+		try {
+			PreparedStatement ps = c.prepareStatement("select a.pk,a.descricao,a.preco,a.quantidade,a.fkusuario,a.titulo from produto a,loteproduto b,leilao c where b.fkleilao=? and b.fkproduto=a.pk");
+			ps.setInt(1,idleilao);
+			ResultSet result = ps.executeQuery();
+			while(result.next()){
+				Produto temp = new Produto();
+				temp.setPk(result.getInt("pk"));
+				temp.setTitulo(result.getString("titulo"));
+				temp.setDescricao(result.getString("descricao"));
+				temp.setPreco(result.getFloat("preco"));
+				temp.setQuantidade(result.getInt("quantidade"));
+				temp.setFkUsuario(result.getInt("fkusuario"));
+				list.add(temp);
+			}
+			result.close();
+			ps.close();
+		
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao listar dados. Classe JDBCLanceDAO", e); 
+		}
+		return list;
 	}
 
 }
