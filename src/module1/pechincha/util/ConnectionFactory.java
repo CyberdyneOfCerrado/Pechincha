d16a5e9ec56c32d54510858466c9e315e8e4fbbf
Guepardo <bsinet@hotmail.com>
2015-10-25 00:07:15 -0200

    Retirando as credenciais do banco de dados.

--- src/module1/pechincha/util/ConnectionFactory.java
+++ src/module1/pechincha/util/ConnectionFactory.java
@@ -6,15 +6,15 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	private final static String  user     = "postgres"; 
-	private final static String  password = "lead9228"; 
-	private final static String  bd       = "pechincha"; 
+	private final static String  password = "122331"; 
+	private final static String  bd       = "123434"; 
 	private static Connection c; 
 	
 	public static Connection getConnection(){
 		if( c == null){
 			try{
 				Class.forName("org.postgresql.Driver"); 
-				return c = DriverManager.getConnection("jdbc:postgresql://g4group.me:5432/"+bd,user,password);
+				return c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/"+bd,user,password);
 			}catch(SQLException | ClassNotFoundException e){
 				throw new RuntimeException("Erro ao retornar a conexão do banco de dados: Classe: ConnectionFactory", e);
 			}
